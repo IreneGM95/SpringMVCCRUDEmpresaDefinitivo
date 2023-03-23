428b48f7e308f5ca7494bd3958834b7a14848037
IreneG <irene.gonza.mor@gmail.com>
2023-03-23 17:01:57 +0100

    Estado inicial

---
+++ src/main/java/com/example/SpringMVCCRUDEmpresaDefinitivo/SpringMvccrudEmpresaDefinitivoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVCCRUDEmpresaDefinitivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvccrudEmpresaDefinitivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvccrudEmpresaDefinitivoApplication.class, args);
+	}
+
+}
