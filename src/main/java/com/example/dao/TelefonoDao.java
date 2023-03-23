9ff91e7cf324aa3c29698bbc8eb96194bba8565b
IreneG <irene.gonza.mor@gmail.com>
2023-03-23 17:45:15 +0100

    Dao creado

---
+++ src/main/java/com/example/dao/TelefonoDao.java
@@ -0,0 +1,25 @@
+package com.example.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.entities.Empleado;
+import com.example.entities.Telefono;
+
+@Repository
+public interface TelefonoDao extends JpaRepository<Telefono, Integer> {
+    
+    // @Query(value = "delete from telefonos where empleado_id = :idEmpleado", nativeQuery = true)
+    // long deleteByEmpleado(@Param("idEmpleado") Integer idEmpleado);
+
+    long deleteByEmpleado(Empleado empleado);
+    
+
+
+    /**Es necesario crear un método que encuentre los telefonso de cada empleado: */
+    List<Telefono> findByEmpleado(Empleado empleado);
+}
