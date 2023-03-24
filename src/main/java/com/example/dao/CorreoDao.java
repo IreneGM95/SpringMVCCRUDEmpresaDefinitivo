1fa7a922a7e88eee66062670fff8f2384107897f
IreneG <irene.gonza.mor@gmail.com>
2023-03-24 09:03:28 +0100

    correo dao corregido

--- src/main/java/com/example/dao/CorreoDao.java
+++ src/main/java/com/example/dao/CorreoDao.java
@@ -3,8 +3,6 @@ package com.example.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.entities.Empleado;
@@ -12,14 +10,13 @@ import com.example.entities.Correo;
 
 @Repository
 public interface CorreoDao extends JpaRepository<Correo, Integer> {
-    
-    // @Query(value = "delete from telefonos where empleado_id = :idEmpleado", nativeQuery = true)
+
+    // @Query(value = "delete from telefonos where empleado_id = :idEmpleado",
+    // nativeQuery = true)
     // long deleteByEmpleado(@Param("idEmpleado") Integer idEmpleado);
 
     long deleteByEmpleado(Empleado empleado);
-    
 
-
-    /**Es necesario crear un método que encuentre los correos de cada empleado: */
+    /** Es necesario crear un método que encuentre los correos de cada empleado: */
     List<Correo> findByEmpleado(Empleado empleado);
 }
